3e361103c7e25ee8c431d53b4791f21bf39d200f
Mstf000 <mostafacrash400@gmail.com>
2024-03-15 01:11:56 +0200

    fixed the register page

--- app.py
+++ app.py
@@ -8,6 +8,25 @@ app.secret_key = 'mysecret'
 app.config['MONGO_URI'] = 'mongodb+srv://mstf:mstf123@cluster0.pfzhwqr.mongodb.net/grad'
 mongo = PyMongo(app)
 
+
+
+@app.route('/getDocLogin', methods=['GET'])
+def getDocLogin():
+        return render_template('index.html', userType="doctor")
+
+@app.route('/getPatientLogin', methods=['GET'])
+def getPatientLogin():
+        return render_template('index.html', userType="patient")
+
+@app.route('/getDocRegister', methods=['GET'])
+def getDocRegister():
+        return render_template('register.html', userType="doctor")
+
+@app.route('/getPatientRegister', methods=['GET'])
+def getPatientRegister():
+        return render_template('register.html', userType="patient")
+
+
 @app.route('/')
 def index():
     if 'username' in session:
@@ -15,47 +34,43 @@ def index():
         return render_template('test.html', username=session['username'])
     else:
         # If user is not logged in, show the login page
-        return render_template('index.html')
+        return render_template('test.html')
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST'])
 def login():
     if request.method == 'POST':
-        users = mongo.db.users
-        login_user = users.find_one({'name': request.form['username']})
+        if request.form["userType"]=='doctor':
+            model = mongo.db.doctors
+        else:
+            model= mongo.db.users
+
+        login_user = model.find_one({'name': request.form['username']})
 
         if login_user:
             input_password = hashlib.sha256(request.form['pass'].encode('utf-8')).hexdigest()
             if input_password == login_user['password']:
                 session['username'] = request.form['username']
-                return redirect(url_for('homepage'))  # Redirect to the homepage (graph.html) after login
+                if request.form["userType"] == 'doctor':
+                    return redirect(url_for('dashboard'))  # Redirect to the homepage (graph.html) after login
+                else:
+                    return redirect(url_for('homepage'))  # Redirect to the homepage (graph.html) after login
         return 'Invalid username/password combination'
     else:
         return render_template('index.html')
 
-@app.route('/loginDoc', methods=['GET', 'POST'])
-def loginDoc():
-    if request.method == 'POST':
-        users = mongo.db.doctors
-        login_user = users.find_one({'name': request.form['username']})
-
-        if login_user:
-            input_password = hashlib.sha256(request.form['pass'].encode('utf-8')).hexdigest()
-            if input_password == login_user['password']:
-                session['username'] = request.form['username']
-                return redirect(url_for('homepage'))  # Redirect to the homepage (graph.html) after login
-        return 'Invalid username/password combination'
-    else:
-        return render_template('loginDOCTORS.html')
-
-@app.route('/register', methods=['POST', 'GET'])
+@app.route('/register', methods=['POST'])
 def register():
     if request.method == 'POST':
-        users = mongo.db.doctors
-        existing_user = users.find_one({'name': request.form['username']})
+        if request.form["userType"] == 'doctor':
+            model = mongo.db.doctors
+        else:
+            model = mongo.db.users
+
+        existing_user = model.find_one({'name': request.form['username']})
 
         if existing_user is None:
             hashpass = hashlib.sha256(request.form['pass'].encode('utf-8')).hexdigest()
-            users.insert_one({'name': request.form['username'], 'password': hashpass})
+            model.insert_one({'name': request.form['username'], 'password': hashpass})
             session['username'] = request.form['username']
             return redirect(url_for('homepage'))  # Redirect to the homepage (graph.html) after registration
         return 'That username already exists!'
@@ -74,6 +89,15 @@ def homepage():
     if 'username' in session:
         # If user is logged in, render the graph.html page
         return render_template('graph.html', username=session['username'])
+    else:
+        # If user is not logged in, redirect them to the login page
+        return redirect(url_for('login'))
+
+@app.route('/upload')
+def upload():
+    if 'username' in session:
+        # If user is logged in, render the graph.html page
+        return render_template('videoUpload.html', username=session['username'])
     else:
         # If user is not logged in, redirect them to the login page
         return redirect(url_for('login'))
